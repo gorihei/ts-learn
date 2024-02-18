00ac0d54699f18c9561c76709e961763e5755bdc
gorihei <gorihei.ff@gmail.com>
2024-02-19 04:38:23 +0900

    20240218

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-const message: string = 'Hello, world!'
-console.log(message)
+type User = {
+  name: string
+  age: number
+  premiumUser: boolean
+}
+
+const data: string = `
+uhyo,26,1
+John Smith,17,0
+Mary Sue,14,1
+`
+const users: User[] = data
+  .split('\n')
+  .filter((row) => row)
+  .map((row) => {
+    const [name, age, premiumUser] = row.split(',')
+    return {
+      name: name,
+      age: Number(age),
+      premiumUser: Boolean(Number(premiumUser)),
+    }
+  })
+
+for (const user of users) {
+  if (user.premiumUser) {
+    console.log(`${user.name}(${user.age})はプレミアムユーザーです。`)
+  } else {
+    console.log(`${user.name}(${user.age})はプレミアムユーザーではありません。`)
+  }
+}
